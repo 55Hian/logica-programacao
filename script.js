87ab46e6616321fd45b4437620a1281e26fafa07
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2023-10-04 14:47:36 +0000

    feat(script): operados logicos, incremento e atribuição. Console log, parseint e prompt) git status

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-document.write("Olá mundoooo!")
+// declarar variavel:
+// const - declara constante (não muda valor)
+// let - declara variaveis em escopo local
+// var - declara variaveis em escopo global
+console.log("Testando o console")
+// problema: receber 3 notas de um aluno e calcular a média; apresentar a média; e o resultado (se aprovado ou não aprovado)
+console.log("\n")
 
-console.log("Olá mundo!")
 
-window.alert("Meu nome é Hian")
+let nota1;
+let nota2;
+let nota3;
+
+let mediaminima = 5;
+
+let media;
+
+console.log("Inicio do processo.");
+
+nota1 = parseInt(prompt("Digite a primeira nota do aluno"));
+nota2 = parseInt(prompt("Digite a segunda nota do aluno"));
+nota3 = parseInt(prompt("Digite a terceira nota do aluno"));
+
+media = (nota1 + nota2 + nota3)/3;
+
+console.log("\n")
+console.log("\t A primeira nota do aluno é: " + nota1)
+console.log("\t A segunda nota do aluno é: " + nota2)
+console.log("\t A terceira nota do aluno é: " + nota3)
+console.log("\n")
+console.log ("\t A nota média do aluno é: " + media);
+console.log("\n")
+
+if (media < mediaminima){
+    console.log("\t O aluno está reprovado!");
+} else {
+    console.log("\t O aluno está aprovado!");
+}
+console.log("\n")
+console.log("Fim do processo.")
